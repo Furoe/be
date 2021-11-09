091bed8a25a1d469ed3ef19c11a97e8cbf59e341
Furo Yang <17751038730@163.com>
2021-11-09 22:48:54 +0800

    提交缺失依赖

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 const Koa = require('koa')
 const koaBody = require("koa-body")
+const BodyParser = require("koa-bodyparser")
 const Router = require('@koa/router')
 const { logger, loggerMiddleware} = require('./middleware/logger')
 
 const app = new Koa()
 const router = new Router()
+
+app.use(loggerMiddleware)
+
+
 app.use(koaBody())
-app.use(loggerMiddleware())
+
+app.use(BodyParser())
 
 router.get('/', async (ctx: { body: { hello: string } }) => {
   ctx.body = {
