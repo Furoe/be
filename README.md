73e9d4c64a9d1e3eb1f1278ee028a2f9fcecac31
Furoe <17751038730@163.com>
2021-11-10 09:55:58 +0800

    修正日志中间件

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### 技术栈
-* koa2
-* typescript
+* 使用`koa2`作为http封装，编程使用`typescript`。
+* 规范校验`eslint`。
+* 使用`nodemon`监测文件修改
+* 日志使用`log4js`
+### 后续
+[ ] 日志进库，如何处理高吞吐？日志管理系统
--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,3 +1,3 @@
 const path = require('path')
 
-export const LogPath = path.resolve(__dirname, '../../logs/koa.log')
+export const LogPath = path.resolve(__dirname, '../../logs/log.log')
